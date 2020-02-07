cfc2f4d0c62ab11ef90da7531c733f1782e9f11f
Volodymyr Korol <volodymyr01korol@gmail.com>
2020-02-07 19:15:46 +0200

    Gulp set

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
+const watch = require('gulp-watch')
+
+gulp.task('sass-compile',()=>{
+    return gulp.src('./wp-content/themes/wp-shop-theme/sass/style.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./wp-content/themes/wp-shop-theme/'))
+})
+gulp.task('watch',()=>{
+    gulp.watch('./wp-content/themes/wp-shop-theme/sass/style.sass',gulp.series('sass-compile'))
+})
